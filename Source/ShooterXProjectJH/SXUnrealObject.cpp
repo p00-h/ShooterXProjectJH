6925bf03c6bca00c636f9c2245bc85889b8c8fb8
p00-h <149079533+p00-h@users.noreply.github.com>
2025-11-11 20:51:20 +0900

    Add SXUnrealObject 생성

---
+++ Source/ShooterXProjectJH/SXUnrealObject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SXUnrealObject.h"
+
+SXUnrealObject::SXUnrealObject()
+{
+}
+
+SXUnrealObject::~SXUnrealObject()
+{
+}
